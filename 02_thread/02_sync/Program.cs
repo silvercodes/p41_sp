d32fb45716b27f31fae3a2c330ec87107479f065
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2026-01-24 22:55:35 +0300

    add:sync

---
+++ 02_thread/02_sync/Program.cs
@@ -0,0 +1,204 @@
+﻿
+// Инструменты синхронизации
+
+// 1. Простые методы блокировки (Thread.Sleep(), Thread.Join(), Task.Wait()....)
+
+// 2. Контроль критических секций (lock, Monitor(20нс), Mutex(1000нс), SpinLock, Semaphore(1000нс), SemaphoreSlim.....)
+
+// 3. Инструменты сигнализации (Monitor.Pulse(), Monitor.PulseAll(), .Wait(), AutoResetEvent, ManualResetEvent, CountdownResetEvent....)
+
+// 4. Неблокирующие инструменты (Thread.MemoryBarrier, Interlocked, Thread.VolitileRead....)
+
+
+#region lock / Monitor (эксклюзивная блокировка)
+
+// Разблокировка
+// 1. Выполнение условий блокировки
+// 2. Таймаут
+// 3. Thread.Interrupt()
+// 4. Thread.Abort()
+
+
+//new Thread(ThreadUnsafe.Run).Start();
+//ThreadUnsafe.Run();
+
+//class ThreadUnsafe
+//{
+//    static int a = 10;
+//    static int b = 20;
+
+//    public static void Run()
+//    {
+//        int c = 0;
+
+//        if (b!= 0)
+//        {
+//            c = a / b;
+//        }
+
+//        b = 0;
+//    }
+//}
+
+
+//class ThreadSafe
+//{
+//    static int a = 10;
+//    static int b = 20;
+//    static object locker = new object();
+
+//    public static void Run()
+//    {
+//        int c = 0;
+
+//        // FIFO
+//        lock(locker)
+//        {
+//            if (b != 0)
+//            {
+//                c = a / b;
+//            }
+
+//            b = 0;
+//        }
+//    }
+//}
+
+
+
+
+//class ThreadSafe
+//{
+//    static int a = 10;
+//    static int b = 20;
+//    static object locker = new object();
+
+//    public static void Run()
+//    {
+//        int c = 0;
+//        bool flag = false;
+
+//        try
+//        {
+//            // FIFO
+//            Monitor.Enter(locker, ref flag);    // Попытка взятия блокировки
+
+//            if (b != 0)
+//            {
+//                c = a / b;
+//            }
+
+//            b = 0;
+//        }
+//        catch(Exception ex)
+//        {
+//            Console.WriteLine($"ERROR: {ex.Message}");
+//        }
+//        finally
+//        {
+//            if (flag)
+//                Monitor.Exit(locker);           // Освобождение блокировки
+//        }
+//    }
+//}
+
+
+
+//object locker = new object();
+//int val = 0;
+//void Run()
+//{
+//    bool flag = false;
+
+//    try
+//    {
+//        flag = Monitor.TryEnter(locker, 1200);
+//        if (flag)
+//        {
+//            for (int i = 0; i < 10; ++i)
+//            {
+//                Console.WriteLine($"{Thread.CurrentThread.Name}: {val++}");
+//                Thread.Sleep(100);
+//            }
+//        }
+
+//        Console.WriteLine($"{Thread.CurrentThread.Name} is looser");
+
+//    }
+//    catch (Exception ex)
+//    {
+//        Console.WriteLine($"ERROR: {ex.Message}");
+//    }
+//    finally
+//    {
+//        if (flag)
+//            Monitor.Exit(locker);           // Освобождение блокировки
+//    }
+//}
+
+//for (int i = 0; i < 3; ++i)
+//{
+//    Thread t = new Thread(Run)
+//    {
+//        Name = $"Thread_{i}"
+//    };
+//    t.Start();
+//}
+
+#endregion
+
+
+#region Mutex
+
+//int count = 0;
+//Mutex mutex = new Mutex();
+
+//void UseResource()
+//{
+//    if (mutex.WaitOne(500))         // Попытка взятия блокировки
+//    {
+//        Console.WriteLine($"{Thread.CurrentThread.Name} takes the mutex");
+
+//        Thread.Sleep(200);
+//        count++;
+
+//        Console.WriteLine($"{Thread.CurrentThread.Name} DONE");
+//        Console.WriteLine($"{Thread.CurrentThread.Name} release mutex");
+
+//        mutex.ReleaseMutex();       // Освобождение мьютекса
+//    }
+//    else
+//    {
+//        Console.WriteLine($"{Thread.CurrentThread.Name} is looser");
+//    }
+
+//}
+
+//void StartThreads()
+//{
+//    for (int i = 0; i < 5; ++i)
+//    {
+//        Thread t = new Thread(UseResource)
+//        {
+//            Name = $"THREAD_{i}"
+//        };
+//        t.Start();
+//    }
+//}
+//StartThreads();
+
+#endregion
+
+
+#region Semaphore
+
+
+
+
+
+
+
+#endregion
+
+
+
