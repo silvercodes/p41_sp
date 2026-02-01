1f8fbe6eecff7d3bbabde36eb23b912fff46ff58
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2026-02-01 19:06:04 +0300

    add: tpl

--- 02_thread/04_TPL/Program.cs
+++ 02_thread/04_TPL/Program.cs
@@ -122,10 +122,304 @@
 
 #region Возврат значения
 
+//Task<int> t = new Task<int>(() =>
+//{
+//    Thread.Sleep(1000);
+//    return 140;
+//});
+//t.Start();
+////
+////
+////
+//int result = t.Result;                      // BLOCKING
+//Console.WriteLine(result);
 
+//Console.WriteLine("Main finished");
+
+
+
+
+
+//int a = 3;
+//int b = 4;
+
+//int Sum(int x, int y) => x + y;
+
+//Task<int> t = new Task<int>(() => Sum(a, b));
+//t.Start();
+////
+////
+//Console.WriteLine(t.Result);
+
+
+
+
+
+//Task<Thread> t = new Task<Thread>(() =>
+//{
+//    Thread thread = new Thread(() => Console.WriteLine("test from thread"));
+
+//    return thread;
+//});
+//t.Start();
+////
+////
+//Thread thread = t.Result;
+//thread.Start();
+
+#endregion
+
+
+
+
+#region Цепочки
+
+using System.Text;
+
+
+//Photo photo = new Photo();
+
+//Task<Photo> t = new Task<Photo>(() =>
+//{
+//    Task<Photo> t1 = new Task<Photo>(() =>
+//    {
+//        Thread.Sleep(1000);
+//        photo.Filters.Add("filter_1");
+
+//        return photo;
+//    });
+//    t1.Start();
+//    Photo result = t1.Result;           // BLOCKING
+
+//    Task t2 = new Task(() =>
+//    {
+//        Thread.Sleep(1500);
+//        result.Filters.Add("filter_2");
+//    });
+//    t2.Start();
+//    t2.Wait();                          // BLOCKING
+
+//    Task t3 = new Task(() =>
+//    {
+//        Thread.Sleep(2000);
+//        result.Filters.Add("filter_3");
+//    });
+//    t3.Start();
+//    t3.Wait();                          // BLOCKING
+
+//    return result;
+//});
+
+//t.Start();
+////
+////
+//Photo result = t.Result;
+//Console.WriteLine(result);
+
+
+
+
+
+//Photo photo = new Photo();
+
+//Task<Photo> t1 = new Task<Photo>(() =>
+//{
+//    Thread.Sleep(1000);
+//    photo.Filters.Add("filter_1");
+
+//    return photo;
+//});
+
+//Task<Photo> taskResult = t1.ContinueWith((t) =>
+//{
+//    Photo res = t.Result;
+//    res.Filters.Add("filter_2");
+
+//    return res;
+//}).ContinueWith((t) =>
+//{
+//    Photo res = t.Result;
+//    res.Filters.Add("filter_3");
+
+//    return res;
+//});
+
+//t1.Start();
+////
+//Photo resPhoto = taskResult.Result;     // BLOCKING
+//Console.WriteLine(resPhoto);
+
+
+
+//Task t1 = new Task(() => Console.WriteLine("start task"));
+
+//Task chain = t1
+//    .ContinueWith(t => Console.WriteLine($"{t.Id} {Thread.CurrentThread.ManagedThreadId}"))
+//    .ContinueWith(t => Console.WriteLine($"{t.Id} {Thread.CurrentThread.ManagedThreadId}"))
+//    .ContinueWith(t => Console.WriteLine($"{t.Id} {Thread.CurrentThread.ManagedThreadId}"))
+//    .ContinueWith(t => Console.WriteLine($"{t.Id} {Thread.CurrentThread.ManagedThreadId}"))
+//    .ContinueWith(t => Console.WriteLine($"{t.Id} {Thread.CurrentThread.ManagedThreadId}"))
+//    .ContinueWith(t => Console.WriteLine($"{t.Id} {Thread.CurrentThread.ManagedThreadId}"))
+//    .ContinueWith(t => Console.WriteLine($"{t.Id} {Thread.CurrentThread.ManagedThreadId}"))
+//    .ContinueWith(t => Console.WriteLine($"{t.Id} {Thread.CurrentThread.ManagedThreadId}"))
+//    .ContinueWith(t => Console.WriteLine($"{t.Id} {Thread.CurrentThread.ManagedThreadId}"));
+
+//t1.Start();
+//chain.Wait();
+
+
+
+//class Photo
+//{
+//    public List<string> Filters { get; set; } = new List<string>();
+//    public override string ToString()
+//    {
+//        StringBuilder sb = new StringBuilder();
+//        Filters.ForEach(f => sb.Append($"{f} "));
+
+//        return sb.ToString();
+//    }
+
+
+//}
+
+#endregion
+
+
+#region Parallel
+
+//Parallel.Invoke(
+//    TestPrint,
+//    () => Console.WriteLine("test lambda"),
+//    () => Sum(3, 4)
+//);
+
+//void TestPrint()
+//{
+//    Thread.Sleep(1000);
+//    Console.WriteLine("Test string");
+//}
+
+//int Sum(int a, int b) => a + b;
+
+
+
+
+//ThreadPool.SetMinThreads(20, 2);
+
+//long time = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+
+//Parallel.For(0, 20, i =>
+//{
+//    Console.WriteLine($"{Task.CurrentId}: {i}");
+//    Thread.Sleep(1000);
+//});
+
+//Console.WriteLine($"TIME: {DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond - time}");
+
+
+
+
+
+
+
+
+//ThreadPool.SetMinThreads(10, 2);
+
+//long time = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+
+//List<int> nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+//Parallel.ForEach(nums, n =>
+//{
+//    Thread.Sleep(1000);
+//    Console.WriteLine(n);
+//});
+
+//Console.WriteLine($"TIME: {DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond - time}");
 
 
 
 #endregion
+
+
+
+#region CancellationToken
+
+// === Мягкое прерывание :-)
+
+//CancellationTokenSource cts = new CancellationTokenSource();
+//CancellationToken token = cts.Token;
+
+//Task t = new Task(() =>
+//{
+//    for (int i = 0;  i < 10; i++)
+//    {
+//        if (token.IsCancellationRequested)
+//        {
+//            Console.WriteLine("Cancellation requsted");
+//            return;
+//        }
+//        Console.WriteLine(i);
+//        Thread.Sleep(1000);
+//    }
+//}, token);
+
+//t.Start();
+
+//Console.ReadLine();
+//cts.Cancel();
+
+//Console.ReadLine();
+
+
+
+
+// === Жёсткое прерывание :-|
+
+
+//CancellationTokenSource cts = new CancellationTokenSource();
+//CancellationToken token = cts.Token;
+
+//Task t = new Task(() =>
+//{
+//    for (int i = 0; i < 10; i++)
+//    {
+//        if (token.IsCancellationRequested)
+//        {
+//            token.ThrowIfCancellationRequested();           // throw TaskCancelledException
+//        }
+//        Console.WriteLine(i);
+//        Thread.Sleep(1000);
+//    }
+//}, token);
+
+//try
+//{
+//    t.Start();
+//    Console.ReadLine();
+//    cts.Cancel();
+//}
+//catch (AggregateException ex)
+//{
+//    foreach (Exception e in ex.InnerExceptions)
+//    {
+//        if (e is TaskCanceledException)
+//            Console.WriteLine("Task interrupted!");
+//        else
+//            Console.WriteLine(e.Message);
+//    }
+//}
+//finally
+//{
+//    cts.Dispose();
+//}
+
+//Thread.Sleep(1000);
+//Console.WriteLine(t.Status);
+//Console.ReadLine();
+
+#endregion
+
 
 
